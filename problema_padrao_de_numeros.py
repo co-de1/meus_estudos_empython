8d1379272172a1adb30becf48ae09071d9aa3122
José <jmantonio2000@gmail.com>
2025-01-02 02:15:50 -0300

    Comitando um exercicio de lógica e praticando a criação de uma branch

---
+++ problema_padrao_de_numeros.py
@@ -0,0 +1,14 @@
+# Treinando lógica
+
+
+def padrao_numero():
+    for i in range(len(lista)):
+        print(lista)
+        lista.pop()
+
+
+numero = input("Informe um numero: ")
+lista = list(numero)
+# print(len(lista))
+
+padrao_numero()
