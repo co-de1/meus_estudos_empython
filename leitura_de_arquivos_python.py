9bfc43f0ae944d3484fc7f857dd90ce99d4ca0a4
José <jmantonio2000@gmail.com>
2025-01-04 14:18:08 -0300

    Adicionando novos arquivos a branch

--- leitura_de_arquivos_python.py
+++ leitura_de_arquivos_python.py
@@ -10,7 +10,6 @@ print(arquivo.read())
 arquivo.seek(0)
 arquivo.close()
 
-"""
 caminho_arquivo = "README.md"  # Substitua pelo caminho do arquivo
 try:
     with open(caminho_arquivo, 'r', encoding='utf-8') as arquivo:
@@ -18,3 +17,33 @@ try:
             print(linha.strip())  # Imprime cada linha
 except FileNotFoundError:
     print("Arquivo não encontrado!")
+
+with open('teste.txt', 'w') as arq:
+    arq.write("Fazendo exercicios da aula escrevendo em arquivos\n")
+    arq.write('Procurando melhorar cada vez mais as minhas habilidades em Python\n')
+
+    arquivo = open('vamos.txt', 'w', encoding='utf-8')
+
+arquivo.write('Vamos com tudo nesta, temos que conseguir alcançar nossos objetivos')
+arquivo.write('\nFoi desta, que 2025 seja um ano de mais foco')
+
+try:
+    with open('teste.txt', 'a+', encoding='utf-8') as arq:
+        arq.write("Fazendo exercicios da aula escrevendo em arquivos\n")
+        arq.write('Procurando melhorar cada vez mais as minhas habilidades em Python\n')
+except FileExistsError:
+    print('Arquivo já existe')
+
+"""
+
+from io import StringIO
+
+mensagem = 'Fzendo testes com o modulo StringIo'
+arq = StringIO(mensagem)
+
+print(arq.read())
+
+arq.write('\tFunciona para escrever outro texto')
+arq.seek(0)
+
+print(arq.read())
